1ebd98976cfef927950b310ca5221e685a8c585d
arufiand <alfiandannyarmanta@gmail.com>
2024-11-01 18:09:20 +0700

    ci: Init Repo + creating Screens

---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget{
+  const LoginScreen({super.key});
+
+  @override
+  createState(){
+    return LoginScreenState();
+  }
+}
+
+class LoginScreenState extends State<LoginScreen>{
+  @override
+  Widget build(context){
+    return Container(
+      margin: const EdgeInsets.all(20.0),
+      child : const Text("Login Screen!!")
+    );
+  }
+}
