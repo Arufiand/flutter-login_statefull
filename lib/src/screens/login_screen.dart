efc20d52b3ad05ad45f87f66e1a1b4926cd3ed93
arufiand <alfiandannyarmanta@gmail.com>
2024-11-02 10:40:11 +0700

    feat: Layout for field and button

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -10,11 +10,48 @@ class LoginScreen extends StatefulWidget{
 }
 
 class LoginScreenState extends State<LoginScreen>{
+
   @override
   Widget build(context){
     return Container(
       margin: const EdgeInsets.all(20.0),
-      child : const Text("Login Screen!!")
+      child : Form(
+        child: Column(
+          children: [
+          emailField(),
+          passwordField(),
+          Container(margin: const EdgeInsets.only(top: 25.0),),
+          submitButton()
+        ],
+        ),
+      )
+    );
+  }
+
+  Widget emailField(){
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: const InputDecoration(
+        labelText: 'Email Label',
+        hintText: 'Email Hint',
+
+      ),
+    );
+  }
+  Widget passwordField(){
+    return TextFormField(
+      obscureText: true,
+      decoration: const InputDecoration(
+        labelText: 'Password Label',
+        hintText: 'Password Hint',
+      ),
+    );
+  }
+
+  Widget submitButton(){
+    return ElevatedButton(
+      child : const Text('Submit'),
+      onPressed : () {}
     );
   }
 }
